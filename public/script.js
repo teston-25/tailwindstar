a882a56c49a182fdbf746403494152dd43e5e40d
leul tesfu <leultesfu25@gmail.com>
2024-09-19 14:28:46 +0300

    'version-modiefied-2'

--- public/script.js
+++ public/script.js
@@ -1,6 +1,27 @@
 const login = document.querySelector('.login');
 const signup = document.querySelector('.signup');
+const home = document.querySelector('.home');
+const about = document.querySelector('.about');
+const contact = document.querySelector('.contact');
+const btnHam = document.querySelector('.btn-ham');
+const btnClose = document.querySelector('.btn-close');
 
+
+home.addEventListener('click',function(){
+    window.location.href="index.html";
+})
+
+about.addEventListener('click',function(){
+    window.location.href="about.html";
+})
+
+contact.addEventListener('click',function(){
+    window.location.href="contacts.html";
+})
+
+btnHam.addEventListener('click',function(){
+    
+})
 
 login.addEventListener('click',function(){
 
