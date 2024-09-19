f5c584bf09267f7e5e3452210d6309791f6d8efa
leul tesfu <leultesfu25@gmail.com>
2024-09-19 17:07:50 +0300

    'version-mod-3'

--- public/script.js
+++ public/script.js
@@ -5,6 +5,7 @@ const about = document.querySelector('.about');
 const contact = document.querySelector('.contact');
 const btnHam = document.querySelector('.btn-ham');
 const btnClose = document.querySelector('.btn-close');
+const nav = document.querySelector('.nav');
 
 
 home.addEventListener('click',function(){
@@ -20,7 +21,20 @@ contact.addEventListener('click',function(){
 })
 
 btnHam.addEventListener('click',function(){
-    
+    btnClose.classList.add('block');
+    btnClose.classList.remove('hidden');
+    btnHam.classList.add('hidden');    
+    btnHam.classList.remove('block');
+    nav.classList.add('block');
+    nav.classList.remove('hidden');
+})
+btnClose.addEventListener('click',function(){
+    btnClose.classList.remove('block');
+    btnClose.classList.add('hidden');
+    btnHam.classList.remove('hidden');    
+    btnHam.classList.add('block');
+    nav.classList.remove('block');
+    nav.classList.add('hidden');
 })
 
 login.addEventListener('click',function(){
